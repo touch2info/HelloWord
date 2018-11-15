8c9b1f031d46f95e8f3dd3029fa8c01af4623b01
admin <admin@LT-1.fios-router.home>
2018-11-14 20:17:30 -0500

    checking from PC

---
+++ HelloWorldGitProject/src/com/touch2info/StartProject.java
@@ -0,0 +1,10 @@
+package com.touch2info;
+
+public class StartProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Why it took so long.... ");
+	}
+
+}
