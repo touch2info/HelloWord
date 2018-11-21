d9ca1cbb7f8d874442531c5632df04b6e72ae56b
Anshul Daga <anshuldaga@gmail.com>
2018-11-21 13:35:05 -0500

    More Coding
    I went over the following topics:
    -for loop
    -for loop nested examples
    -while loop
    -do-while loop
    -breaks

--- HelloWorldGitProject/src/com/touch2info/AnimalStartProject.java
+++ HelloWorldGitProject/src/com/touch2info/AnimalStartProject.java
@@ -6,13 +6,16 @@ public class AnimalStartProject {
 	//IMP: Can't write Sys out directly in class. It has to go in method. 
 	//System.out.println("test before main"); 
 	
-	private int i;
+	//instance variable (outside the method)
+	private int i = 5;
 	private String firstName = "defaultValue to first name variable";
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("*********** Start of the application ******");
 		
+		//local variable 
+		int i = 10;
 		AnimalStartProject animalStartProject = new AnimalStartProject();
 		System.out.println("firstName BEFORE calling set method:"+animalStartProject.getFirstName());
 		animalStartProject.setFirstName("New first name");
@@ -21,6 +24,112 @@ public class AnimalStartProject {
 		System.out.println("Doing one more PR for ashish branch..");
 		System.out.println("Checking from Anshul mac.. some other changes");
 		System.out.println("New changes to add to git branch by anshul");
+		System.out.println("i:"+ i);
+		System.out.println("Set i value from set i method:" + animalStartProject.getI());
+		
+		//if else example
+		
+		int age = 11;
+		
+		if (age>12) {
+			System.out.println("Anshul is older");
+		} else {
+			
+			System.out.println("Anshul is younger");
+		}
+		System.out.println("\n\n");
+		//Switch Example
+		
+		 int number=40;  
+		    //switch expression with int value  
+		    switch(number){  
+		    //switch cases without break statements  
+		    case 10: System.out.println("10");  
+		    break;
+		    case 20: System.out.println("20");  
+		    break;
+		    case 30: System.out.println("30");  
+		    break;
+		    default:System.out.println("Not in 10, 20 or 30"); 
+		    }
+		
+		 System.out.println("\n\n");
+		 
+		 
+		//Example that shows each 'if' statement refers to it's particular 'else' statement on same indent
+		 int years=25;    
+		    int weight=48;      
+		    //applying condition on age and weight    
+		    if(years>=18){      
+		        if(weight>50){    
+		            System.out.println("You are eligible to donate blood");    
+		        } else{  
+		            System.out.println("You are not eligible to donate blood");    
+		        }  
+		    } else{  
+		      System.out.println("Age must be greater than 18");
+		    }
+		  System.out.println("\n\n");
+		  
+		//Code of Java for simple for loop 
+		//The syntax is (initialization, condition, inc/decr)
+		    for(int x=1;x<=5;x++){  
+		        System.out.println(x);  
+		    }
+		      
+		    System.out.println("\n\n");
+		   
+		//Declaring an array  
+		    int arr[]={12,23,44,56,78};  
+		//Printing array using for-each loop  
+		    for(int y:arr){  
+		        System.out.println(y);  
+		    }
+		    
+		    System.out.println("\n\n");
+		    
+		 /*Confusing... So the first for loop executes with i1=1 and the second one with j=1
+		  * It continues on to println (output is 1 1) because 'break aa' is only executed when i1 and j = 2
+		  * What's key is that the first for loop executes as i1=1 but then the second loop keeps executing
+		  * through its sequence as j=1, j=2, j=3 until it hits 4 and that's when the condition is false
+		  * so it goes back to the first loop where i1=2 now and then j=1 and continues on the j=2
+		  * BUT now i1 and j are both =2 which executes the break aa code and then stops the first loop 
+		  * therefore stopping the second nest loop as well
+		  * Question: Where exactly does the second loop go once the condition is false? - how does it know to go back to first loop
+		  */
+		    aa:  
+		        for(int i1=1;i1<=3;i1++){  
+		            bb:  
+		                for(int j=1;j<=3;j++){  
+		                    if(i1==2&&j==2){  
+		                        break aa;  
+		                    }  
+		                    System.out.println(i1+" "+j);
+		                }  
+		        }  
+		    
+		    System.out.println("\n\n");
+		    
+		    /* This one different because line dd breaks instead of cc
+		     * Breaks second loop when m and n = 2 then goes back to first loop where m=3 now
+		     * Notice it goes from 2 1 to 3 1 and skips 2 3 even though 2 3 is not 2 2
+		     *Qstn:What would be the code for "do nothing" instead of breaking the loop so that 2 3 still pops up?
+		     *Ansr: continue;
+		     */
+		    cc:  
+		        for(int m=1;m<=3;m++){  
+		            dd:  
+		                for(int n=1;n<=3;n++){  
+		                    if(m==2&&n==2){  
+		                        break dd;  
+		                    }  
+		                    System.out.println(m+" "+n);  
+		                }  
+		        }  
+		    
+		    //Not going to do it here but if you put for(;;){ it creates an infinitive loop
+		    //Question: if you don't define a variable as private or public, what is the default?
+		    
 	}
 
 	public int getI() {
---
+++ HelloWorldGitProject/src/com/touch2info/JavaLearning.java
@@ -0,0 +1,64 @@
+package com.touch2info;
+
+public class JavaLearning {
+	
+	public static void main(String[] args) {
+		
+		//while loop syntax while(condition){code...}
+		
+		  int i=1;  
+		    while(i<=3){  
+		        System.out.println(i);  
+		    i++;  
+		    }  
+		    
+		    
+		    System.out.println("\n");
+		    
+		//if you put while(true) then it will create an infinite loop
+		    
+		/*the do-while loop is same as while loop except it executes the code at least once bc 
+		 * condition is checked after the first iteration
+		 */
+		int j=1;
+		
+		do {
+			System.out.println(j);
+		j++;
+		}while(j<=3);
+		
+		//you can put "true" after while to make do-while loop infinitive too
+		
+		System.out.println("\n");
+		
+		//Using the continue statement
+		
+		 //for loop  
+	    for(int k=1;k<=4;k++){  
+	        if(k==2){  
+	            //using continue statement  
+	            continue;//it will skip the rest statement  
+	        }  
+	        System.out.println(k);  
+		
+	    }
+		
+	  //outer loop  
+        for(int m=1;m<=3;m++){    
+                //inner loop  
+                for(int n=1;n<=3;n++){    
+                    if(m==2&&n==2){    
+                        //using continue statement inside inner loop  
+                        continue;    
+                    }    
+                    System.out.println(m+" "+n);    
+                }    
+        }    
+		
+		
+		
+		
+		
+	}
+
+}
